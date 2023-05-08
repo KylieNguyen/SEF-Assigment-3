32fb8883e1a30124f0f72172b24e2d22f107e9ad
Kylie Nguyen <s3946026@student.rmit.edu.au>
2023-05-08 19:42:46 +1000

    small beginning of poll class

--- Poll.java
+++ Poll.java
@@ -1,5 +1,10 @@
 //Scenario: A manager creates a poll that is responded to by a trainee
 
+import java.util.List;
+
 public class Poll {
     
+    private List<String> questions;
+    private List<String> options;
+    //private Map<String, int> results;
 }
